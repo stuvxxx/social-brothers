91f716cfe2ea2388881301b4a8a4c6fef278b7cd
Marc Zwiers <mczwiers@gmail.com>
2022-10-31 14:37:42 +0100

    First run!

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav"
+import Logo from "./Logo"
+import "../styles/header.css"
+
+const Header = () => {
+    return (
+      <header>
+        <Logo />
+        <Nav />
+      </header>
+    )
+  };
+  
+  export default Header;
