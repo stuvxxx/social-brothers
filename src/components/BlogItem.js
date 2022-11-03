0caf451f08d2253901f7f4d4ef4567fa6ae86391
Marc Zwiers <mczwiers@gmail.com>
2022-11-03 06:47:02 +0100

    new day

---
+++ src/components/BlogItem.js
@@ -0,0 +1,31 @@
+import "../styles/blogs-item.css"
+import rect from "../assets/Mask-1.png"
+
+
+const imgBaseUrl = "https://frontend-case-api.sbdev.nl/storage/"
+
+const removeTime = (date) => {
+  const newDate = date.split("T")
+  return newDate[0]
+}
+
+
+
+const Blogs = ({post}) => {
+    return (
+      <div className="blogs-item">
+        <img src={imgBaseUrl + post.img_url} alt="a" />
+        <img src={rect} />
+        <div className="date-cat">
+          <p>{removeTime(post.updated_at)}</p>
+          <p>{post.category.name}</p>
+        </div>
+        <h3>{post.title}</h3>
+        <div className="article-wrapper">
+          <p>{post.content}</p>
+        </div>
+      </div>
+  )
+  };
+  
+  export default Blogs;
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,12 +1,50 @@
 import "../styles/blogs.css"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import BlogItem from "./BlogItem"
+import Loading from "./Loading"
 
 
 const Blogs = () => {
+  const [posts, setPosts] = useState([])
+  const [pages, setPages] = useState("4")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get("https://frontend-case-api.sbdev.nl/api/posts?page=&perPage=" + pages, {
+      headers: {
+        "token": "pj11daaQRz7zUIH56B9Z"
+      }
+    })
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+  }, [])
+
     return (
       <div className="blogs-wrapper">
-        hai
+        <div className="blogs-item-wrapper">
+        { loading
+          ? <Loading />
+          : posts.data.map((post) => {
+            return (
+              <BlogItem post={post} key={post.id}/>
+            )
+          })}
+        </div>
       </div>
   )
   };
+
+
   
   export default Blogs;
+
